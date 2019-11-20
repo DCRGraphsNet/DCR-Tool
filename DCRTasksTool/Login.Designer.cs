1a8db4b0833657dea3dd0f67c7f842bf7043efdc
Ahtesham Ul Haq <Ahtesham.Haq@teo-intl.com>
2019-11-20 15:47:24 +0500

    new release changes

---
+++ DCRTasksTool/Login.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DCRTasksTool
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelLB = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(159, 104);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(202, 20);
+            this.textBoxUserName.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(159, 156);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(202, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPassword_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(214, 211);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "UserName";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.labelLB);
+            this.panel2.Location = new System.Drawing.Point(0, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(493, 77);
+            this.panel2.TabIndex = 8;
+            // 
+            // labelLB
+            // 
+            this.labelLB.AutoSize = true;
+            this.labelLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLB.Location = new System.Drawing.Point(130, 13);
+            this.labelLB.Name = "labelLB";
+            this.labelLB.Size = new System.Drawing.Size(215, 44);
+            this.labelLB.TabIndex = 0;
+            this.labelLB.Text = "DCR TOOL";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 262);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUserName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelLB;
+    }
+}
---
+++ DCRTasksTool/TaskList.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using DCRTasksTool.AppCode;
+
+namespace DCRTasksTool
+{
+    public partial class TaskList : Form
+    {
+
+        string GraphID = string.Empty;
+        string SimID = string.Empty;
+        List<Events> eveLst = new List<Events>();
+
+        public TaskList(string id)
+        {
+            InitializeComponent();
+            GraphID = id;
+           
+        }
+
+        public void OnLoad(bool isSuccess)
+        {
+            if (isSuccess)
+            {
+                RoundButton start = new RoundButton();
+                start.Name = "rb1";
+                start.Location = new System.Drawing.Point(710, 100);
+                start.Size = new System.Drawing.Size(50, 50);
+                start.BackColor = System.Drawing.Color.Green;
+                this.Controls.Add(start);
+                checkIsAccepting();
+            }
+            else
+            {
+
+                var choice = new Choice();
+                this.Hide();
+                choice.Closed += (s, args) => this.Close();
+                choice.Show();
+            }
+
+        }
+
+        public TaskList()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("please select event to execute");
+                return;
+            }
+            string selectItemText = listView1.SelectedItems[0].SubItems[6].Text;
+            executeWithRepository(GraphID, SimID, selectItemText);
+        }
+
+
+
+        private void executeWithRepository(string graphID, string simID, string eventName)
+        {
+            string retrunXML = string.Empty;
+            List<Events> lst = FunctionalityFromRepository.GetExecutedEvents(graphID, simID, eventName, ref retrunXML);
+            if (string.IsNullOrWhiteSpace(retrunXML) == false)
+            {
+                string errorDetail = string.Empty;
+                bool chhk = HelpingFunctions.checkSuccessExecute(retrunXML, ref errorDetail);
+                if (chhk)
+                {
+                    MessageBox.Show("Something went wrong , please try later ");
+                }
+                else
+                {
+                    string err = HelpingFunctions.updateErrorMsgReplaceIdWithLabels(eveLst, errorDetail);
+                    MessageBox.Show(err);
+                    //MessageBox.Show(errorDetail);
+                }
+
+            }
+            else
+            {
+                LoadEvent(lst);
+                checkIsAccepting();
+            }
+        }
+
+        private bool GetEvents(string id)
+        {
+            bool isSuccess = false;
+            string error = string.Empty;
+
+            List<Events> lst = FunctionalityFromRepository.GetAllEvents(id, ref SimID, ref error);
+            if (string.IsNullOrWhiteSpace(error))
+            {
+                isSuccess = true;
+                LoadEvent(lst);                
+            }
+            else
+            {
+                MessageBox.Show(error);
+            }
+            return isSuccess;
+        }
+
+        private void LoadEvent(List<Events> lst)
+        {
+            eveLst = lst;
+            listView1.Items.Clear();
+            foreach (Events item in lst.OrderByDescending(o => o.IsIncluded).ThenByDescending(o => o.IsPending).ThenByDescending(o => o.IsEnabled).ThenBy(o => o.IsExecuted))
+            {
+                string showPending = string.Empty;
+                if (item.IsIncluded == "true" && item.IsPending == "true")
+                {
+                    showPending = "!";
+                }
+                else if (item.IsExecuted == "true")
+                {
+                    showPending = "\u2714"; //((char)0x221A).ToString();
+                }
+                ListViewItem lvi = new ListViewItem(showPending);
+                lvi.UseItemStyleForSubItems = false;
+                lvi.SubItems.Add(item.EventLabel);
+                lvi.SubItems.Add(item.IsIncluded);
+                lvi.SubItems.Add(item.IsEnabled);
+                lvi.SubItems.Add(item.IsPending);
+                lvi.SubItems.Add(item.IsExecuted);
+                lvi.SubItems.Add(item.EventID);
+                if (item.IsIncluded == "true" && item.IsPending == "true")
+                {
+                    lvi.SubItems[0].ForeColor = Color.Red;
+                    lvi.SubItems[0].Font = new System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold);
+                }
+                else if (item.IsExecuted == "true")
+                {
+                    lvi.SubItems[0].ForeColor = Color.Green;
+                    lvi.SubItems[0].Font = new System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold);
+                }
+                listView1.Items.Add(lvi);
+            }
+
+        }
+
+        private void checkIsAccepting()
+        {
+            bool accepted = false;
+            accepted = FunctionalityFromRepository.GetIsAccepting(GraphID, SimID);
+            if (accepted)
+            {
+                showButton(false);
+            }
+            else
+            {
+                showButton(true);
+            }
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var taskList = new Choice();
+            taskList.Closed += (s, args) => this.Close();
+            taskList.Show();
+        }
+
+        private void showButton(bool isred)
+        {
+            RoundButton rb = this.Controls.Find("rb1", true).FirstOrDefault() as RoundButton;
+
+            if (rb != null)
+            {
+                if (isred)
+                {
+                    ColorConverter cc = new ColorConverter();
+                    Color color = (Color)cc.ConvertFromString("#03A1FC");
+                    rb.BackColor = color;
+                }
+                else
+                {
+                    Color result = Color.FromArgb(0, 248, 0);
+                    rb.BackColor = result;
+                }
+            }
+
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            string error = string.Empty;
+            LoadEvent(FunctionalityFromRepository.GetLatestEvents(GraphID, SimID, ref error));
+        }
+
+        private void TaskList_Shown(object sender, EventArgs e)
+        {
+            bool isSuccess = GetEvents(GraphID);
+            OnLoad(isSuccess);
+        }
+    }
+}
